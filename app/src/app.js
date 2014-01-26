5e677a8317a4b409c16617f2ae0176bdc2a8f804
Brian Vanderbusch <brian@2pioneers.com>
2014-01-25 22:41:56 -0600

    add layout for chart

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by chief on 1/18/14.
  */
-var debate = angular.module('debate',['ui.router']);
+var debate = angular.module('debate',['ui.router','googlechart']);
 
 
 debate
@@ -27,6 +27,154 @@ debate
     })
     .controller('navCtrl',function($scope){
 
+    })
+    .controller('chartCtrl',function($scope){
+        $scope.chart = {
+            "type": "PieChart",
+            "displayed": true,
+            "cssStyle": "height:600px; width:100%;",
+            "data": {
+                "cols": [
+                    {
+                        "id": "month",
+                        "label": "Month",
+                        "type": "string",
+                        "p": {}
+                    },
+                    {
+                        "id": "laptop-id",
+                        "label": "Laptop",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "desktop-id",
+                        "label": "Desktop",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "server-id",
+                        "label": "Server",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "cost-id",
+                        "label": "Shipping",
+                        "type": "number"
+                    },
+                    {
+                        "id": "",
+                        "role": "tooltip",
+                        "type": "string",
+                        "p": {
+                            "role": "tooltip",
+                            "html": true
+                        }
+                    }
+                ],
+                "rows": [
+                    {
+                        "c": [
+                            {
+                                "v": "January"
+                            },
+                            {
+                                "v": 19,
+                                "f": "42 items"
+                            },
+                            {
+                                "v": 12,
+                                "f": "Ony 12 items"
+                            },
+                            {
+                                "v": 7,
+                                "f": "7 servers"
+                            },
+                            {
+                                "v": 4
+                            },
+                            {
+                                "v": " <b>Shipping 4</b><br /><img src=\"http://icons.iconarchive.com/icons/antrepo/container-4-cargo-vans/512/Google-Shipping-Box-icon.png\" style=\"height:85px\" />"
+                            }
+                        ]
+                    },
+                    {
+                        "c": [
+                            {
+                                "v": "February"
+                            },
+                            {
+                                "v": 13
+                            },
+                            {
+                                "v": 1,
+                                "f": "1 unit (Out of stock this month)"
+                            },
+                            {
+                                "v": 12
+                            },
+                            {
+                                "v": 2
+                            },
+                            {
+                                "v": " <b>Shipping 2</b><br /><img src=\"http://icons.iconarchive.com/icons/antrepo/container-4-cargo-vans/512/Google-Shipping-Box-icon.png\" style=\"height:85px\" />"
+                            }
+                        ]
+                    },
+                    {
+                        "c": [
+                            {
+                                "v": "March"
+                            },
+                            {
+                                "v": 24
+                            },
+                            {
+                                "v": 5
+                            },
+                            {
+                                "v": 11
+                            },
+                            {
+                                "v": 6
+                            },
+                            {
+                                "v": " <b>Shipping 6</b><br /><img src=\"http://icons.iconarchive.com/icons/antrepo/container-4-cargo-vans/512/Google-Shipping-Box-icon.png\" style=\"height:85px\" />"
+                            }
+                        ]
+                    }
+                ]
+            },
+            "options": {
+                "title": "Sales per month",
+                "isStacked": "true",
+                "fill": 20,
+                "displayExactValues": true,
+                "vAxis": {
+                    "title": "Sales unit",
+                    "gridlines": {
+                        "count": 8
+                    }
+                },
+                "hAxis": {
+                    "title": "Date"
+                },
+                "tooltip": {
+                    "isHtml": true
+                }
+            },
+            "formatters": {},
+            "view": {
+                "columns": [
+                    0,
+                    1,
+                    2,
+                    4
+                ]
+            }
+        }
     })
 
 .directive('closebtn',function(){
