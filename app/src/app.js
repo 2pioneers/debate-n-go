92391fe57e3a2d5d6425eb5c8c31de2d29d56a7c
Brian Vanderbusch <brian@2pioneers.com>
2014-01-26 01:33:43 -0600

    working placement of d3 chart

--- app/src/app.js
+++ app/src/app.js
@@ -29,7 +29,31 @@ debate
 
     })
     .controller('chartCtrl',function($scope){
+        $scope.exampleData = [
+                 	{ key: "One", y: 5 },
+                     { key: "Two", y: 2 },
+                     { key: "Three", y: 9 },
+                     { key: "Four", y: 7 },
+                     { key: "Five", y: 4 },
+                     { key: "Six", y: 3 },
+                     { key: "Seven", y: 9 }
+                ];
+        $scope.xFunction = function(){
+            return function(d) {
+                return d.key;
+            };
+        }
+        $scope.yFunction = function(){
+            return function(d) {
+                return d.y;
+            };
+        }
 
+        $scope.descriptionFunction = function(){
+            return function(d){
+                return d.key;
+            }
+        }
     })
 
 .directive('closebtn',function(){
